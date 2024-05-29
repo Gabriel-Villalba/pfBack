000a16510278e193591d87186896c08908d8f27f
Carla <capasoan@hotmail.com>
2024-05-28 19:09:45 -0500

    Primeros cambios

---
+++ src/Models/Ursers.js
@@ -0,0 +1,37 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+    sequelize.define('User', {
+        id: {
+            type: DataTypes.UUID,
+            primaryKey: true,
+            allowNull: false
+        },
+        Nombre: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        Email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        },
+        Contraseña: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        Direccion: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        Telefono: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        Fecha_de_registro: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW
+        }
+    }, { timestamps: false });
+}
