4aca9b762fe86adfd9444d3bd55a09e5068f8789
Carla <capasoan@hotmail.com>
2024-06-18 16:12:39 -0500

    cambios en rutas

--- src/Routes/usersRouter.js
+++ src/Routes/usersRouter.js
@@ -1,17 +1,9 @@
-const { Router } = require("express");
-const usersRouter = Router()
-const { } = require("../controllers/users")
-//const { check } = require("express-validator")
+const express = require('express');
+const router = express.Router();
 
-// usersRouter.post("/create", registerHandler)// crear usuario
-// usersRouter.post("/login", loginHandler)// accede al sistema
+router.get('/', (req, res) => {
+    res.send('Users route');
+});
 
-
-
- 
-// usersRouter.get ("/", getUsers) // listar usuarios
-// usersRouter.put ("/:id" , updateUser) // modificar usuarios
-// usersRouter.delete ("/:id" , deleteUser) // borrado logico
-
-module.exports = usersRouter
+module.exports = router;
 
