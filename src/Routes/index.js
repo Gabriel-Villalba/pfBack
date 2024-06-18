12e2353b8314a869b183d90120db97718f8400b8
Carla <capasoan@hotmail.com>
2024-06-18 17:39:57 -0500

    cambios index routes

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -1,9 +1,13 @@
 const { Router } = require("express");
-const users = require("./usersRouter.js");
-const product = require("./productRouter.js");
-const products = require("./productsRouter.js");
-const categories = require("./categoriesRouter.js");
-const login = require("./loginRouter.js");
+const users = require ("./usersRouter.js")
+const product = require ("./productRouter.js")
+const products = require ("./productsRouter.js")
+const categories = require ("./categoriesRouter.js")
+//const sales = require ("./salesRouter.js")
+//const stock = require ("./stockRouter.js")
+//const record = require ("./recordRoutes.js")
+const login = require ("./loginRouter.js")
+//const report = require ("./reportRouter.js")
 
 const router = Router();
 
@@ -12,9 +16,13 @@ router.get('/', (req, res) => {
 });
 
 router.use('/users', users);
-router.use('/product', product);
-router.use('/products', products);
+router.use('/product', product)
+router.use('/products', products)
 router.use('/categories', categories);
+//router.use('/sales', sales);//ventas
+//router.use('/stock',stock);
+//router.use('/record',record);// registro
 router.use('/login', login);
+//router.use('/report',report);// reportes
 
-module.exports = router;
+module.exports = router
