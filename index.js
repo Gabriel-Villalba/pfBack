224c0f1c281e3df9e40bee906e3a494d5cbf1e4d
Carla <capasoan@hotmail.com>
2024-06-28 14:25:09 -0500

    subiendo cambios carrito a deploy

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
-require('dotenv').config();
-const server = require('./src/app.js');
-const { conn } = require('./src/db.js');
-const PORT = process.env.PORT || 3001; 
+require("dotenv").config();
+const server = require("./src/app.js");
+const { conn } = require("./src/db.js");
+const PORT = process.env.PORT || 3001;
 
 // console.log('Environment Variables:', process.env);
 // console.log('Current Directory:', __dirname);
 // Sync the database and start the server
-conn.sync()
+//Probando con cambios para subir a nueva
+conn
+  .sync()
   .then(() => {
     server.listen(PORT, () => {
       console.log(`Server listening at ${PORT}`);
     });
   })
-  .catch(err => {
-    console.error('Error syncing database:', err);
+  .catch((err) => {
+    console.error("Error syncing database:", err);
     if (err.parent) {
-      console.error('Parent error:', err.parent);
+      console.error("Parent error:", err.parent);
     }
-  }); 
-
+  });
