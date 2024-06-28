8a696a378ba8222858e54b46a3ffa950fff5d8ef
Carla <capasoan@hotmail.com>
2024-06-28 14:49:52 -0500

    subiendo cambios carrito a deploy3

--- src/db.js
+++ src/db.js
@@ -20,10 +20,10 @@ const ProductCart = require("./Models/ProductCart");
 // console.log('DB_HOST:', process.env.DB_H
 // console.log('DB_NAME:', process.env.DB_NAME);
 
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_DEPLOY } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME } = process.env;
 
 const sequelize = new Sequelize(
-  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/tienda_de_ropa`,
+  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,
   {
     logging: false, // set to console.log to see the raw SQL queries
     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
